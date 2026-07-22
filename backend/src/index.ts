f0c2e186ad4b088ce1da55e82c8bf1a3bca5ae38
David Card <davidd.card05@gmail.com>
2026-07-21 18:08:22 -0700

    add Express server scaffold with TS

---
+++ backend/src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('server running');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
+
+
+
+
+
+
+
+
+
